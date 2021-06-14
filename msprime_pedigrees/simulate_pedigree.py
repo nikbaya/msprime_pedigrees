3178676530863ed2f9bba8bb80a0806905fb2607
nbaya <nikolasbaya@gmail.com>
2021-06-14 09:03:16 +0100

    updated pedigree simulator

--- msprime_pedigrees/simulate_pedigree.py
+++ msprime_pedigrees/simulate_pedigree.py
@@ -9,74 +9,75 @@ N_CHILDREN_PROB = np.asarray(N_CHILDREN_CTS) / sum(N_CHILDREN_CTS)
 
 PERCENT_W_PARTNER = 0.5
 
-def choose_sex():
-    sex = 1 + np.random.binomial(n=1, p=0.5)  # 1 = male, 2 = female
-    return sex
+def simulate_pedigree(
+        tc: tskit.TableCollection,
+        n_founders = 8, 
+        n_children_prob = [0, 0, 2],
+        n_generations = 3,
+        random_sex=True):
 
-def choose_n_children(n_children_prob):
-    return np.random.choice(a=len(n_children_prob), p=n_children_prob)
-
-def simulate_pedigree_from_founders():
-    def is_valid_pair(pat_anc, mat_anc, mat_idx):
-        common_anc = set(pat_anc).intersection(mat_anc)
-        return len(common_anc) == 0
-    np.random.seed(5)
-
-    n_founders = 10000
+    def choose_sex(random=True, sex=None):
+        # male = 1, female = 2
+        if random:
+            sex = np.random.binomial(n=1, p=0.5) + 1
+        else: # non-random: alternate male female to ensure equal balance
+            sex = (sex % 2) + 1 
+        return sex
+    
+    def choose_n_children(n_children_prob, n_families=1):
+        return np.random.choice(
+            a=len(n_children_prob), 
+            size=n_families, 
+            p=n_children_prob)
+    
+    tb = tc.individuals
+    tb.metadata_schema = tskit.MetadataSchema(
+        {
+            "codec": "json",
+            "type": "object",
+            "properties": {
+                "sex": {"type": "integer"},
+            },
+            "required": ["sex"],
+            "additionalProperties": True,
+        }
+    )
+   
+    if random_sex:
+        sex=None
+    else:
+        sex = 1 # male=1, female=2
+    
+    # first generation
     n_males = int(n_founders / 2)
+    curr_gen = [range(n_males), range(n_males, n_founders)]
+    for ind in range(n_founders):
+        tb.add_row(
+            parents=[-1, -1],
+            metadata={'sex': (ind>=n_males)+1}
+        )
 
-    # n_children_prob = [0.2, 0.2, 0.2, 0.2, 0.2]
-    n_children_prob = [0, 0, 0, 1]
-
-    n_total = n_founders
-
-    # set up
-    curr_gen = [[], []]
-    sex = 1
-    for pat in range(n_males):
-        mat = n_males + pat
-        n_children = choose_n_children(n_children_prob=n_children_prob)
-        for _ in range(n_children):
-            curr_gen[sex] += [[[pat, mat]]]
-            sex = (sex + 1) % 2
-
-    n_generations = 10
-    max_gen_idx = 2  # max_gen_idx=2 -> 3 generations back
-    for _ in range(n_generations - 1):
-        n_males = len(curr_gen[0])
-        n_females = len(curr_gen[1])
-        # print(n_males, n_females)
+    # next generations            
+    for gen_idx in range(2, n_generations+1):
         next_gen = [[], []]
-        avail_mat = np.random.permutation(n_females)
-        for pat_idx in range(n_males):
-            pat = pat_idx
-            pat_anc = curr_gen[0][pat_idx][-1]
-            for mat_idx in avail_mat:
-                mat_anc = curr_gen[1][mat_idx][-1]
-                if is_valid_pair(pat_anc, mat_anc, mat_idx):
-                    break
-            if is_valid_pair(pat_anc, mat_anc, mat_idx) and len(
-                    avail_mat) > 0:  # unrelated male/female pair has been made
-                avail_mat = avail_mat[avail_mat != mat_idx]
-                n_children = choose_n_children(n_children_prob=n_children_prob)
-                n_total += n_children
-                mat = mat_idx + n_males
-                anc = [[pat, mat]]
-                # number of generations for paternal and maternal ancestors
-                # should be the same
-                for gen_idx in range(
-                        min(max_gen_idx, len(curr_gen[0][pat_idx]))):
-                    pat_anc = curr_gen[0][pat_idx][gen_idx]
-                    mat_anc = curr_gen[1][mat_idx][gen_idx]
-                    anc += [pat_anc + mat_anc]
-                for _ in range(n_children):
-                    next_gen[sex] += [anc]
-                    sex = (sex + 1) % 2
-            # else:
-            #     print(f'no match for pat={pat}')
-        # print(next_gen[0])
-        # print(next_gen[1])
+        avail_pat = np.random.permutation(curr_gen[0])
+        avail_mat = np.random.permutation(curr_gen[1])
+        n_pairs = min(len(curr_gen[0]), len(curr_gen[1]))
+        if n_pairs==0 and n_children_prob[0]!=1:
+            raise Exception(f"Not enough people to make children in generation {gen_idx}")
+        pairs = zip(avail_pat[:n_pairs], avail_mat[:n_pairs])
+        n_children_per_pair = choose_n_children(
+            n_children_prob=n_children_prob,
+            n_families=n_pairs
+        )
+        for (pat, mat), n_children in zip(pairs, n_children_per_pair):
+            for _ in range(n_children):
+                sex = choose_sex(
+                    random=random_sex, 
+                    sex=sex)
+                next_gen[sex-1] += [len(tb)]
+                tb.add_row(
+                    parents=[pat, mat],
+                    metadata={'sex': sex}
+                )
         curr_gen = next_gen
-
-        print(f'current gen: {len(curr_gen[0])+len(curr_gen[1])}')
-    print(f'n_total: {n_total}')
